994e8e07093b143a044401416209a0567ea9a082
ubermachine <shivamkatoch@outlook.com>
2020-01-29 01:20:53 +0530

    basic markdown finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import ReactDOM from 'react-dom'
-import './App.css';
-import Quotes from './components/quotes'
+import './App.scss';
+import Markdown from './components/markdown'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import $ from "jQuery";
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Quotes/>
+        <Markdown/>
       </header>
     </div>
   );
