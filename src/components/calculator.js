512a315bd942d48ae8f57510186e473f0e388ed8
ubermachine <shivamkatoch@outlook.com>
2020-02-06 17:42:36 +0530

    one test left

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,6 @@
 import React  from 'react'
-let decflag=0
+let decflag1=0
+let decflag2=0
 let zerflag=0
 class Calculator extends React.Component{
     constructor(props){
@@ -14,33 +15,122 @@ class Calculator extends React.Component{
         
     
     }
-    getInput(event){
-        const numtest=/[\d]/
-        const dec=/[.+]/
-        const functest=/[/*\-+]/
-        const zertest=/^0/
-        if(numtest.test(event.target.innerHTML)){
-            if(this.state.current[0]=='0' && event.target.innerHTML=='0'){
-                this.setState({
-                    current:this.state.current
-                })
-               
-            }else{this.setState({
-                current:this.state.current+event.target.innerHTML
-            })}
 
-                
-        }else if(dec.test(event.target.innerHTML) && decflag==0){
-            decflag=1
+    getInput(event){
+
+        const numtest=/[\d]+/g
+        const dec=/[.]/
+        const functest=/[/*\-+]/
+
+        if(numtest.test(event.target.innerHTML)){
+            if(!this.state.func||this.state.func=="="){
+            if((event.target.innerHTML==="0"  &&(this.state.output == "0"))){
             this.setState({
-                current:this.state.current+event.target.innerHTML
+                current:parseFloat(this.state.current+event.target.innerHTML),
+                output:parseFloat(this.state.current+event.target.innerHTML)
+
+            })   
+        }else{this.setState({
+            current:this.state.current+event.target.innerHTML,
+            output:(this.state.current+event.target.innerHTML)
+        }) 
+    }
+    }
+            else{if((event.target.innerHTML==="0" )&&(this.state.output == "0")){
+                this.setState({
+                    nextip:parseFloat(this.state.nextip+event.target.innerHTML),
+                    output:parseFloat(this.state.nextip+event.target.innerHTML)
+    
+                }) 
+            }else{this.setState({
+                nextip:this.state.nextip+event.target.innerHTML,
+                output:(this.state.nextip+event.target.innerHTML)
+            })    
+            }
+
+            }              
+                
+        }else if(dec.test(event.target.innerHTML) && decflag1===0 &&(!this.state.func||this.state.func=="=")){
+            decflag1=1
+            this.setState({
+                current:this.state.current+event.target.innerHTML,
+                output:(this.state.current+event.target.innerHTML)
             })
         
             
-        }else if(functest.test(event.target.innerHTML)){
-
+        }else if(dec.test(event.target.innerHTML) && decflag2===0 ){
+            decflag2=1
+            this.setState({
+                nextip:this.state.nextip+event.target.innerHTML,
+                output:(this.state.nextip+event.target.innerHTML)
+            })
+        
+            
+        }else if(event.target.id==="clear"){
+            this.setState({
+                current:'',
+                nextip:'',
+                func:'',
+                output:0
+            })
+            decflag1=0;
+            decflag2=0;
+        }
+        else if(functest.test(event.target.innerHTML)&&!this.state.nextip){
+           
+            this.setState({
+                func:event.target.innerHTML,
+            }
+            )
         
     }
+    else if (event.target.innerHTML==="=" || (event.target.innerHTML!="." &&functest.test(event.target.innerHTML)&&this.state.nextip)){
+        let a=parseFloat(this.state.current);
+        let b=parseFloat(this.state.nextip);
+        switch(this.state.func){
+            case "*":
+                this.setState({
+                    output:a*b,
+                    current:a*b,
+                    nextip:'',
+                    func:event.target.innerHTML
+                    
+                })
+            break;
+            case "/":
+                this.setState({
+                    output:a/b,
+                    current:a/b,
+                    nextip:'',
+                    func:event.target.innerHTML
+                    
+                })
+                break;
+            case "+":
+                this.setState({
+                    output:a+b,
+                    current:a+b,
+                    nextip:'',
+                    func:event.target.innerHTML
+                    
+                })
+            break;
+            case "-":
+                this.setState({
+                    output:a-b,
+                    current:a-b,
+                    nextip:'',
+                    func:event.target.innerHTML
+                   
+                })
+            break;
+            default:                
+
+
+
+        }
+    }
+
 
     }
     render(){
@@ -48,7 +138,7 @@ class Calculator extends React.Component{
             <div id="caldisplay">
             <button onClick={this.getInput} id="clear">C</button>
             <button id="equals" onClick={this.getInput}>=</button>
-            <div id="display">{this.state.current}</div>
+            <div id="display">{(this.state.output)}</div>
             <button onClick={this.getInput} id="one">1</button>
             <button onClick={this.getInput} id="two">2</button>
             <button  onClick={this.getInput} id="three">3</button>
