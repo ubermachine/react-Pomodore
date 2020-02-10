cca850298aeeb78bbb4a0ee5435e1c46af02f369
ubermachine <shivamkatoch@outlook.com>
2020-02-11 03:30:55 +0530

    Updates

---
+++ precache-manifest.28f30c2960b7d8dec3a3503bea00d052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4965e2642728c20a3f8fe663b1fad8c",
+    "url": "/react-Pomodore/index.html"
+  },
+  {
+    "revision": "a4d52367884c967a6d50",
+    "url": "/react-Pomodore/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5172459cf815684adab1",
+    "url": "/react-Pomodore/static/css/main.d152f07c.chunk.css"
+  },
+  {
+    "revision": "a4d52367884c967a6d50",
+    "url": "/react-Pomodore/static/js/2.7d1ea56c.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/react-Pomodore/static/js/2.7d1ea56c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5172459cf815684adab1",
+    "url": "/react-Pomodore/static/js/main.a95f3708.chunk.js"
+  },
+  {
+    "revision": "3c766f8f2d4887b7b8fa",
+    "url": "/react-Pomodore/static/js/runtime-main.707326c1.js"
+  }
+]);
